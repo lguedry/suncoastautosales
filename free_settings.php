42b5e503d137e0b4c16feb8df39cc9d4bac41fd2
Chris Naus <NausDesign@gmail.com>
2016-10-17 19:10:59 -0500

    site upload
    added fresh copy of site

---
+++ free_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "lynnette.guedry@suncoastautosales.net"; // your email address
+$email_subject = "Contact from Co-Op Form Message"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
